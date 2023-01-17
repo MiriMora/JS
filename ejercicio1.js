5def7eba25acc796030263bac3b9072fb5e8b185
Miriam Martinez Mora <miri8mora@gmail.com>
2023-01-17 14:13:23 +0100

    presentacion.js
    primer ejercicio openbootcamp

---
+++ ejercicio1.js
@@ -0,0 +1,12 @@
+const presentacion = {
+    nombre : "Miriam",
+    edad : 28,
+    isDesarrollador: true,
+    libro : {
+        titulo : "Solo una idiota se enamora",
+        editorial: "planeta",
+        autor : "Nohewi"
+    }, 
+}
+const fecha_nacimiento = new Date(23,01,1994)
+console.log(presentacion,fecha_nacimiento)
---
+++ ejercicio2.js
@@ -0,0 +1,14 @@
+
+let str_nombre = ("Miriam")
+let str_apellido = ("Martinez")
+let cadena = ("Es estudiante   ")
+let estudianteMayus = "estudiante"
+let estudianteMinus = "estudiante"
+console.log(str_nombre +" "+ str_apellido+" " + cadena)
+console.log(estudianteMinus.toLowerCase)
+console.log(estudianteMayus.toUpperCase)
+console.log(cadena.length)
+console.log(str_nombre.slice(0,1))
+console.log(str_apellido.slice(7))
+console.log(cadena.trimEnd)
+
