b5e2a3123e40739e3f5bfb9c8e3352aee76ecd40
Miriam Martinez Mora <miri8mora@gmail.com>
2023-01-18 14:32:33 +0100

    horasMinutosSegundos

---
+++ HorasMinutosSegundos.js
@@ -0,0 +1,5 @@
+// Cuantos Minutos,horas y segundos tiene un dia
+const horas = 24
+const minutos = horas*60
+const segundos = minutos*60
+console.log("Horas" +" "+ horas +" "+ "minutos" +" "+ minutos +" "+ "segundos" + " "+ segundos)
