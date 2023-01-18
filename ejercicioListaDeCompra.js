f34885bc0b4ec1aa13858ea8641f9f3583ca076e
Miriam Martinez Mora <miri8mora@gmail.com>
2023-01-18 14:30:37 +0100

    listaDeLacompra

---
+++ ejercicioListaDeCompra.js
@@ -0,0 +1,6 @@
+const listaDeCompra = ["Pan","huevos","salchichas","manzanas","lechuga"]
+console.log(listaDeCompra)
+console.log(listaDeCompra("Aceite de girasol"))
+console.log(listaDeCompra.pop("Aceite de girasol"))
+
+
